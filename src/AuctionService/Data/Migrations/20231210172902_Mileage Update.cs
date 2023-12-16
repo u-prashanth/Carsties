e6967b655257b39d2783a6292adb4c026a32aa5b
Prashanth <uprashanth1994@outlook.com>
2023-12-16 17:56:38 +0530

    auctions microservice

---
+++ src/AuctionService/Data/Migrations/20231210172902_Mileage Update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MileageUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Milage",
+                table: "Items",
+                newName: "Mileage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mileage",
+                table: "Items",
+                newName: "Milage");
+        }
+    }
+}
